1403c2ab3e928a0837fd64ee9328d77c3a2c1105
David E Melnikoff <davidemelnikoff@gmail.com>
2025-11-03 12:59:11 -0800

    Update timeline.js

--- jsFiles/timeline.js
+++ jsFiles/timeline.js
@@ -2,7 +2,7 @@ var filename_prefix = jsPsych.data.getURLVariable('PROLIFIC_PID');
 if (!filename_prefix) { filename_prefix = jsPsych.randomization.randomID(10) };
 var myfilename = filename_prefix + "_combined.csv";
 
-var experiment_id = "VMIphYUhxxh3";
+var experiment_id = "BHGfuTnfq9Pd";
 var prolific_completion_code = "CGHVX8IL";
 
 var condition_assignment;
